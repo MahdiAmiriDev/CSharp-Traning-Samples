ba318dd40bdb8ec1c52ff0fa0a5d41c5e1535e77
unknown <mahdiamirigamefa@gmail.com>
2024-06-06 22:35:29 +0330

    add polly

---
+++ Polly/polly/polly/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+using Newtonsoft.Json;
+using Polly;
+
+var httpClient = new HttpClient();
+var maxRetryAttempts = 3;
+var pauseBetweenFailures = TimeSpan.FromSeconds(2);
+
+var retryPolicy = Policy
+    .Handle<HttpRequestException>()
+    .WaitAndRetryAsync(maxRetryAttempts, i => pauseBetweenFailures);
+
+await retryPolicy.ExecuteAsync(async () =>
+{
+    global::System.Console.WriteLine("test one");
+
+    var response = httpClient
+      .GetStringAsync("https://fakestoreapi.com").Result;
+        var res = JsonConvert.SerializeObject(response);
+});
